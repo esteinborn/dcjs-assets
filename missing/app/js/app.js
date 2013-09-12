c8b63c4f5cd453de3ba078a642cc13e7b35b6768
Eric Steinborn <esteinborn@gmail.com>
2013-09-12 12:23:55 -0400

    move and rename missing app

--- missing/app/js/app.js
+++ missing/app/js/app.js
@@ -6,24 +6,24 @@ angular.module('missingapp', ['missingapp.directives', 'missingapp.filters', 'ng
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/', {
-        templateUrl: '/js/mpcapp/partials/default.html'}).
+        templateUrl: 'partials/default.html'}).
       when('/missing-child', {
-        templateUrl: '/js/mpcapp/partials/missing-list-child.html',
+        templateUrl: 'partials/missing-list-child.html',
         controller: MissingCtrl}).
       when('/missing-child/:name', {
-        templateUrl: '/js/mpcapp/partials/missing-detail-child.html',
+        templateUrl: 'partials/missing-detail-child.html',
         controller: MissingCtrl}).
       when('/missing-vulnerable-adult', {
-        templateUrl: '/js/mpcapp/partials/missing-list-adult.html',
+        templateUrl: 'partials/missing-list-adult.html',
         controller: MissingCtrl}).
       when('/missing-vulnerable-adult/:name', {
-        templateUrl: '/js/mpcapp/partials/missing-detail-adult.html',
+        templateUrl: 'partials/missing-detail-adult.html',
         controller: MissingCtrl}).
       when('/alerts', {
-        templateUrl: '/js/mpcapp/partials/missing-list-alert.html',
+        templateUrl: 'partials/missing-list-alert.html',
         controller: MissingCtrl}).
       when('/alerts/:name', {
-        templateUrl: '/js/mpcapp/partials/missing-detail-alert.html',
+        templateUrl: 'partials/missing-detail-alert.html',
         controller: MissingCtrl}).
       otherwise({redirectTo: '/'});
 }]);
